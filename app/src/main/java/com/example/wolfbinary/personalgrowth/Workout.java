72e806d69ebfb48f1d4eb9fb312bf8bbe681ba1e
Cameron Stoner <wolfbinary@gmail.com>
2017-04-25 20:42:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/wolfbinary/personalgrowth/Workout.java
@@ -0,0 +1,13 @@
+package com.example.wolfbinary.personalgrowth;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
